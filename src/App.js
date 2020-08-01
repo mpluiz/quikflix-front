fb41654675f47eaa641bb4c652cb3d696f39062f
mpluiz <marcospaulo.luiz@gmail.com>
2020-08-01 20:43:01 -0300

    Initial configuration of routes to Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
-  return (<h1>Hello Quikflix!</h1>);
+  return (
+    <>
+      <Routes />
+    </>
+  );
 }
 
 export default App;
