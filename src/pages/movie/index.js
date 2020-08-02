ba1b16c1a236a12c4e0d81923bdb5afb447947ce
mpluiz <marcospaulo.luiz@gmail.com>
2020-08-01 21:10:07 -0300

    Initial configuration of routes to Movie page

---
+++ src/pages/movie/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Movie() {
+  return <h1>Hello Movie!</h1>;
+}
+
+export default Movie;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/home';
+import Movie from '../pages/movie';
 
 function Routes() {
   return(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={Movie} />
       </Switch>
     </BrowserRouter>
   )
